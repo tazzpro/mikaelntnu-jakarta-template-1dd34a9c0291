90444b7a884d4799de95425054cfa6bd2d5157c3
Rasmus Drønnen <rasmussd@stud.ntnu.no>
2019-09-25 22:24:50 +0200

    Done some more fixes

--- src/main/java/no/ntnu/tollefsen/template/FindService.java
+++ src/main/java/no/ntnu/tollefsen/template/FindService.java
@@ -5,20 +5,34 @@
  */
 package no.ntnu.tollefsen.template;
 
+import java.io.InputStream;
 import java.math.BigDecimal;
+import java.rmi.server.UID;
 import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import no.ntnu.tollefsen.template.domain.Group;
 import no.ntnu.tollefsen.template.domain.Item;
 import no.ntnu.tollefsen.template.domain.User;
 import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.glassfish.jersey.media.multipart.ContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 /**
  *
@@ -47,8 +61,14 @@ public class FindService {
     }
     
     @GET
-    @Path("all")
+    @Path("getall")
     public List<Item> getAll() {
         return ent.createQuery("Select i from Item i", Item.class).getResultList();
     }
+    
+    
+    
+    
+    
+    
 }
--- src/main/java/no/ntnu/tollefsen/template/ItemService.java
+++ src/main/java/no/ntnu/tollefsen/template/ItemService.java
@@ -6,16 +6,25 @@
 package no.ntnu.tollefsen.template;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import no.ntnu.tollefsen.template.domain.Group;
 import no.ntnu.tollefsen.template.domain.Item;
+import no.ntnu.tollefsen.template.domain.User;
 import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 /**
  *
@@ -30,9 +39,29 @@ public class ItemService {
     @Inject
     JsonWebToken principal;
     
-    @GET
-    public List<Item> getAll() {
-        return ent.createQuery("Select i from Item i", Item.class).getResultList();
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response NewUser(@FormDataParam("firstname")String firstname,
+            @FormDataParam("lastname")String lastname,
+            @FormDataParam("street")String street,
+            @FormDataParam("city")String city,
+            @FormDataParam("postalcode")int postalcode,
+            @FormDataParam("email")String email,
+            @FormDataParam("uid")String uid,
+            @FormDataParam("pwd")String pwd)
+    {
+        User user;
+        
+        try{
+            user = new User();
+            System.out.println("Users:" + ent.createQuery("select u from User u", User.class).getResultList());
+        }catch(Exception e)
+        {
+            Logger.getLogger(FindService.class.getName()).log(Level.SEVERE, null, e);
+            return Response.serverError().build();
+        }
+        return Response.ok().build();
     }
     
 }
